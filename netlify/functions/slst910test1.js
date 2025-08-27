aa878d89fbc0ec7d678876ba6b842cf926342136
Samir <123217312+samirchmandal@users.noreply.github.com>
2025-08-27 22:20:28 +0530

    Update slst910test1.js

--- netlify/functions/slst910test1.js
+++ netlify/functions/slst910test1.js
@@ -3,17 +3,17 @@ exports.handler = async (event, context) => {
         {
             question: "Which one of the following is necessarily an invertible matrix?",
             options: ["A nilpotent matrix", "An idempotent matrix", "A symmetric matrix", "An orthogonal matrix"],
-            answer: "An orthogonal matrix"
+            answer: 3
         },
         {
             question: "What is the capital of India",
             options: ["Calcutta", "Mumbai", "New Delhi", "Chennai"],
-            answer: "New Delhi"
+            answer: 2
         },
         {
             question: "Find $\\int_0^1x^3~dx$",
             options: ["$\\frac{1}{3}$","$\\frac{2}{3}$","$\\frac{1}{5}$","$\\frac{1}{4}$"],
-            answer: "$\\frac{1}{4}$"
+            answer: 3
         }
     ];
 
